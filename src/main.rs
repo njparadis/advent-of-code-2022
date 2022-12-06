9ca65429ae934fe4ef21541f6d08bdff9e825e8b
Nate Paradis <nate.paradis@klaviyo.com>
2022-12-06 17:15:25 -0500

    day 3 and then some

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
+use std::collections::HashSet;
 
 fn main() {
-    // day_1();
+    day_1();
     day_2();
+    day_3();
+    day_4();
+    day_5();
+    day_6();
 }
 
 fn day_1() {
@@ -111,6 +116,54 @@ fn day_2() {
     println!("Part 2: {}", score_2);
 }
 
+fn day_3() {
+    println!("Day 3");
+    let score_lookup = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    let mut score_1: usize = 0;
+    let mut score_2: usize = 0;
+    if let Ok(lines) = read_lines("./inputs/day_3.txt") {
+        let mut group: Vec<String> = Vec::new();
+        for line in lines {
+            if let Ok(backpack) = line {
+                group.push(backpack.clone());
+                if group.len() == 3 {
+                    let badge = common_char_two(&group[0], &group[1], &group[2]);
+                    score_2 += score_lookup.find(badge).unwrap() + 1;
+                    group = Vec::new();
+                }
+                let (a, b) = backpack.split_at(backpack.len() / 2);
+                let common = common_char(a, b);
+                score_1 += score_lookup.find(common).unwrap() + 1;
+            }
+        }
+    }
+    println!("Part 1: {}", score_1);
+    println!("Part 2: {}", score_2);
+}
+
+fn common_char(a: &str, b: &str) -> char {
+    let set_a: HashSet<char> = a.chars().collect();
+    let set_b: HashSet<char> = b.chars().collect();
+    *set_a.intersection(&set_b).next().unwrap()
+}
+fn common_char_two(a: &str, b: &str, c: &str) -> char {
+    let set_a: HashSet<char> = a.chars().collect();
+    let set_b: HashSet<char> = b.chars().collect();
+    let set_c: HashSet<char> = c.chars().collect();
+    *set_a.intersection(&set_b).cloned().collect::<HashSet<char>>().intersection(&set_c).next().unwrap()
+}
+
+
+fn day_4() {
+    println!("Day 4");
+}
+fn day_5() {
+    println!("Day 5");
+}
+fn day_6() {
+    println!("Day 6");
+}
 // The output is wrapped in a Result to allow matching on errors
 // Returns an Iterator to the Reader of the lines of the file.
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
